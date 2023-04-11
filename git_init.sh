ba9c758cc7a9217f8157592d83f5a8d090485676
betterme <betterme@bettermedeMacBook-Pro.local>
2023-04-11 12:36:19 +0800

    init

--- git_init.sh
+++ git_init.sh
@@ -5,7 +5,7 @@ git init
 git add *
 git commit -m "init"
 
-#git remote add origin git@github.com:yuanjie-ai/llm4gpt.git
-#git branch -M master
-git push --force-with-lease  origin master
+git remote add origin git@github.com:yuanjie-ai/llm4gpt.git
+git branch -M master
+git push -u origin master
 # git remote remove origin
