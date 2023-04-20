81227dc887edc87585135f14b360b19800700ed0
betterme <betterme@bettermedeMacBook-Pro.local>
2023-04-20 19:30:23 +0800

    init

--- git_init.sh
+++ git_init.sh
@@ -5,8 +5,8 @@
 git add *
 git commit -m "init"
 
-git remote add origin git@github.com:yuanjie-ai/llm4gpt.git
-git branch -M master
+#git remote add origin git@github.com:yuanjie-ai/llm4gpt.git
+#git branch -M master
 
 git pull
 git push -u origin master
--- llm/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from llm.models import ChatLLM
