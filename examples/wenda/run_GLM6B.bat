1aa25151573c31e9b9daf8e2fc1cdba1bc980334
betterme <betterme@bettermedeMacBook-Pro.local>
2023-04-20 19:03:58 +0800

    init

--- examples/wenda/run_GLM6B.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call settings.bat
-set llm_type=glm6b
-:a
-"%PYTHON%" wenda.py
-goto a
-pause
-exit /b
--- examples/wenda/run_llama.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call settings.bat
-set llm_type=llama
-:a
-%PYTHON% wenda.py
-goto a
-pause
-exit /b
